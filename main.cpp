4579a3b33827970a6ea189980cef2e465024c3e0
Erkan Ozhan <erkanozhan@gmail.com>
2025-03-22 10:48:49 +0300

    TR

--- main.cpp
+++ main.cpp
@@ -2,13 +2,13 @@
 #include <omp.h>
 
 int main() {
-    printf("Parallel execution starts now!\n");
+    printf("Paralel calisma simdi basliyor!\n");
 
 #pragma omp parallel
     {
         int thread_id = omp_get_thread_num();
         double omp_time = omp_get_wtime();
-        printf("Thread %d started execution at OMP time: %f seconds\n", thread_id, omp_time);
+        printf("Thread %d calismaya basladi OMP time: %f seconds\n", thread_id, omp_time);
     }
 
     return 0;
